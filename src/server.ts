482ef19fed9bf315e5a582e42cd3114df25e7cf0
Arthur Reis <reisarthur260205@gmail.com>
2026-09-13 16:09:06 -0300

    implement rate limit

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import cors from "@fastify/cors";
+import rateLimit from "@fastify/rate-limit";
 import swagger from "@fastify/swagger";
 import scalar from "@scalar/fastify-api-reference";
 import { fastify } from "fastify";
@@ -15,6 +16,17 @@ const app = fastify({ logger: true }).withTypeProvider();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 app.register(cors, { origin: true });
+
+app.register(rateLimit, {
+	max: 20,
+	timeWindow: "1 minute",
+	cache: 10000,
+	errorResponseBuilder: (_request, context) => ({
+		statusCode: 429,
+		error: "Too Many Requests",
+		message: `Rate limit exceeded. Retry in ${context.after}.`,
+	}),
+});
 
 app.register(swagger, {
 	openapi: {
